566962b1cfe7f0dc3c1c2cba23ef6e8dbec5b0c1
Joshi <nagjoshi08@gmail.com>
2023-07-04 22:38:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import QuizScreen from './components/QuizScreen';
 import Home from './components/Home';
 import QuizScreenTwo from './components/QuizScreenTwo';
 import QuizScreenThree from './components/QuizScreenThree';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/quizscreen' element={<QuizScreen />} />
-          <Route exact path='/quizscreentwo' element={<QuizScreenTwo />} />
-          <Route exact path='/quizscreenthree' element={<QuizScreenThree />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quizscreen" element={<QuizScreen />} />
+        <Route path="/quizscreentwo" element={<QuizScreenTwo />} />
+        <Route path="/quizscreenthree" element={<QuizScreenThree />} />
+      </Routes>
+    </Router>
   );
 }
 
